21d022ee08b0d54a8e5ded6c6c568a14169bcd82
debian <debian@debian.(none)>
2017-05-04 23:38:13 +0200

    fork() aus Schleife)

--- Betriebssysteme Praktikum 1/main.cpp
+++ Betriebssysteme Praktikum 1/main.cpp
@@ -33,25 +33,27 @@
 
 using namespace std;
 
+int status; //empfängt den Wert von exit() und gibt diesen an wait() weiter
 int pid_index = 0;
 int takt = -1;
 
-struct Befehle{
+struct Befehle {
     vector<string> aCommands;
     vector<string> aValues;
 };
 
-class Process{
+class Process {
 public:
-    Process(int v, int w, int x, Befehle b) 
-        :pid(v), pc(w), ppid(x), befehle(b){
-         blocked = false;
-         start = takt;
+
+    Process(int v, int w, int x, Befehle b)
+    : pid(v), pc(w), ppid(x), befehle(b) {
+        blocked = false;
+        start = takt;
     }
 
     int pid;
-    int ppid;    //process ID
-    int pc;     // program counter
+    int ppid; //process ID
+    int pc; // program counter
     bool blocked;
     int start;
     int total;
@@ -62,9 +64,8 @@ vector<Process> processes_vector;
 vector<Process> ready_processes;
 vector<Process> blocked_processes;
 
-void handler(int x) 
-
-{
+void handler(int x)
+ {
 
     cout << "Bye\n";
 
@@ -72,80 +73,78 @@ void handler(int x)
 
 }
 
-Befehle readFile(string filename){
+Befehle readFile(string filename) {
     string str1, str2;
     Befehle befehle;
     ifstream inFile(filename);
     istream_iterator<string> ii(inFile);
     istream_iterator<string> eos;
 
-    while(ii != eos)
-    {
-        str1 = *ii; ++ii;
-        str2 = *ii; ++ii;
-                    
+    while (ii != eos) {
+        str1 = *ii;
+        ++ii;
+        str2 = *ii;
+        ++ii;
+
         befehle.aCommands.push_back(str1);
         befehle.aValues.push_back(str2);
     }
     return befehle;
 }
 
-void simulateProcess(Process p)
-{
-    Befehle b = p.befehle;
+void simulateProcess(Process p) {                                       //simulateProcess nochmal ohne Parameter "Process p" 
+    Befehle b = p.befehle;                                        
     bool debug = true;
     int pc_counter = p.pc;
-    int integer=0;
-    
-    for(int i = 0; i < b.aCommands.size(); ++i)
-    {
-        if(p.blocked == true){
+    int integer = 0;
+   
+    for (int i = 0; i < b.aCommands.size(); ++i) {
+        
+        if (p.blocked == true) {                                      //möglichst anders als funktion schreiben (beispiel "getstatus()")
             simulateProcess(ready_processes.front());
             continue;
         }
-        takt = takt +1;
+        takt = takt + 1;
         char B;
         int Zahl;
         B = b.aCommands.at(i).at(0);
         string temp;
-        
-        if(typeid(b.aValues.at(i)) == typeid(int))
-        {
+
+        if (typeid (b.aValues.at(i)) == typeid (int)) {
             temp = b.aValues[i];
-            Zahl = stoi(temp);
+            Zahl = stoi(temp);  //Wert direkt aus inputstream als int abspeichern
         }
+
+        switch (B) {
         
-        switch(B)
-        {
             case 'S':
             {
                 integer = Zahl;
-                cout << "Process: " << p.pid << "Zahl=" << integer;
+                cout << "S Process: " << p.pid << "gesetzte Zahl= " << integer;
                 break;
             }
             case 'A':
             {
-                integer = integer+Zahl;
-                cout << "Process: " << p.pid << "Zahl=" << integer;
+                integer = integer + Zahl;
+                cout << "A Process: " << p.pid << "addierte Zahl= " << integer;
                 break;
             }
             case 'D':
             {
-                integer = integer-Zahl;
-                cout << "Process: " << p.pid << "Zahl=" << integer;
+                integer = integer - Zahl;
+                cout << "D Process: " << p.pid << "subtrahierte Zahl= " << integer;
                 break;
             }
             case 'B':
             {
+                cout << "B";
                 p.blocked = true;
                 // push process to blocked process list
                 blocked_processes.push_back(p);
                 // remove process from ready process list
-                
-                for(int i = 0; i < ready_processes.size(); ++i)
-                {
-                    if(ready_processes.at(i).pid == p.pid)
-                    {
+
+                for (int i = 0; i < ready_processes.size(); ++i) {
+                    if (ready_processes.at(i).pid == p.pid) {
                         ready_processes.erase(ready_processes.begin() + i);
                     }
                 };
@@ -155,14 +154,16 @@ void simulateProcess(Process p)
             }
             case 'E':
             {
+                cout << "E";
                 i = b.aCommands.size();
                 break;
             }
             case 'R':
             {
+                cout << "R";
                 Befehle befehleNew;
                 befehleNew = readFile(b.aValues.at(i));
-                Process pNew(pid_index+1, 0,p.pid, befehleNew);
+                Process pNew(pid_index + 1, 0, p.pid, befehleNew);
                 processes_vector.push_back(pNew);
                 ready_processes.push_back(pNew);
                 break;
@@ -173,100 +174,110 @@ void simulateProcess(Process p)
                 break;
             }
         }
+        cout << endl;
     }
 }
 
-void init()
-{
+void init() {
     Befehle befehle;
     befehle = readFile("init.txt");
-    Process p(pid_index, 0,0, befehle);
+    Process p(pid_index, 0, 0, befehle);            //als globale variable? und werte mit settern und gettern machen?
     processes_vector.push_back(p);
     ready_processes.push_back(p);
 
     simulateProcess(p);
 }
 
-int main(int argc, char** argv) 
-
-{
+int main(int argc, char** argv)
+ {
     int n;
     int pipefd[2];
     pid_t pid, pid2;
     char buffer[600] = "";
     
-    while(true)
-    {
-        signal(SIGINT, handler);
-        if (pipe(pipefd) < 0)   //Pipe erstellen
-        {
-            perror ("pipe-error");
-            exit (EXIT_FAILURE);
-        }
+    init();
+   
 
-        if((pid = fork()) < 0)  //forken
-        {
-            perror ("fork-error");
-            exit (EXIT_FAILURE);
-        }
-        else if(pid > 0)    //Kommandant (Parent-Prozess)
-        {
-            close(pipefd[0]);    //Leseseite schließen
+
+    signal(SIGINT, handler);
+    if (pipe(pipefd) < 0) //Pipe erstellen
+    {
+        perror("pipe-error");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((pid = fork()) < 0) //forken
+    {
+        perror("fork-error");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) //Kommandant (Parent-Prozess)
+    {
+        close(pipefd[0]); //Leseseite schließen
+
+
+        while (true) {
+
 
             cout << "$ ";
-            cin.getline(buffer,600,'\n');
-            write(pipefd[1],buffer, sizeof(buffer) - 1);    //in die Pipe schreiben
-            memset(buffer,'\0',600);    //Array leeren
-
-            if((waitpid(pid,0,0)) < 0)   //Auf Child-Prozess warten
-            {
-                perror("waitpid-error");
-                exit (EXIT_FAILURE);
-            }
-        }
-        else    //Prozessmanager (Child-Prozess) erstellt die Reporter-Prozesse
-        {
-            close(pipefd[1]);    //Schreibseite schließen
-
-            n = read(pipefd[0], buffer, sizeof(buffer) - 1);    //aus der Pipe lesen
-            //umwandlung in string zur Überprüfung ob Befehl oder nicht
+            cin.getline(buffer, 600, '\n');
+            write(pipefd[1], buffer, sizeof (buffer) - 1); //in die Pipe schreiben
             string str(buffer);
-            if(str == "Quit") 
-            {
-                cout << "Shell wird beendet" << endl;   //Alle Prozesse killen?
-                kill(getppid(),SIGKILL);  
-                kill(getpid(),SIGKILL);
-                exit (EXIT_SUCCESS);
+//            memset(buffer, '\0', 600); //Array leeren
+
+//            if ((waitpid(pid, 0, 0)) < 0) //Auf Child-Prozess warten
+//            {
+//                perror("waitpid-error");
+//                exit(EXIT_FAILURE);
+//            }
+
+          
+            if (str == "Quit") {
+                cout << "Shell wird beendet" << endl; //Alle Prozesse killen?
+//                kill(getppid(), SIGKILL);
+//                kill(getpid(), SIGKILL);
+                wait(&status);
+                exit(EXIT_SUCCESS);
             }
-            else if(str == "Report")
-            {
-                init();
-                if((pid2 = fork()) < 0)
-                {
-                    cout << "fork-error-child2" << endl;
-                    exit (EXIT_FAILURE);
-                }
-                else if(pid2 == 0)  //Reporter-Prozess
-                {
-                }
-                else    //Prozessmanager
-                {
-                    if((waitpid(pid,0,0)) < 0)   //Auf Reporter-Prozess warten
-                    {
-                        perror("waitpid-error2");
-                        exit (EXIT_FAILURE);
-                    }
-                }
-            }
-            else
-            {
-                write(STDOUT_FILENO, buffer, n);   //auf Konsole ausgeben
-                n = 0;
-                cout << endl;
-                exit (EXIT_SUCCESS);   
-            }
+            
         }
+    } else //Prozessmanager (Child-Prozess) erstellt die Reporter-Prozesse
+    {
+        close(pipefd[1]); //Schreibseite schließen
+
+        n = read(pipefd[0], buffer, sizeof (buffer) - 1); //aus der Pipe lesen
+        //umwandlung in string zur Überprüfung ob Befehl oder nicht
+        string str(buffer);
+        
+        if (str=="Unblock"|| str=="u" || str=="U"){
+               cout<<"Unblock "<< endl;
+                                                                        // über funktion realisieren?
+            }
+        
+        if (str == "Report") {
+
+            if ((pid2 = fork()) < 0) {
+                cout << "fork-error-child2" << endl;
+                exit(EXIT_FAILURE);
+            } else if (pid2 == 0) //Reporter-Prozess
+            {
+                cout << "Report-child" << endl;
+            } else //Prozessmanager
+            {
+                if ((waitpid(pid, 0, 0)) < 0) //Auf Reporter-Prozess warten
+                {
+                    perror("waitpid-error2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        } else {
+            write(STDOUT_FILENO, buffer, n); //auf Konsole ausgeben
+            n = 0;
+            cout << endl;
+            exit(EXIT_SUCCESS);
+        }
+        
     }
+
     return 0;
 }
 
