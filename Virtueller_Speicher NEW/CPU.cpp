f33ced9c780fb78be4ddf7ba92d6ec416037affe
Jonas Bernard <job244@arcor.de>
2017-06-29 15:57:56 +0200

    Failure Corrected

--- Virtueller_Speicher NEW/CPU.cpp
+++ Virtueller_Speicher NEW/CPU.cpp
@@ -6,23 +6,23 @@
 CPU::CPU()
 {
 
-	RAM = new Arbeitsspeicher;
-	mmu = new MMU(RAM);
+    RAM = new Arbeitsspeicher;
+    mmu = new MMU(RAM);
 
 
-	for (int i = 0; i < Anzahl_der_Prozesse; i++)
-	{
+    for (int i = 0; i < Anzahl_der_Prozesse; i++)
+    {
             Prozesse.push_back(new Prozess(mmu->get_os()->get_hdd()));  //Prozesse initialisieren
-	}
+    }
 
         
         //Ersten aktiven Prozess setzen
         
-	aktiver_Prozess = Prozesse.at(rand() % Anzahl_der_Prozesse);   //aktiven Prozess für CPU setzen
+    aktiver_Prozess = Prozesse.at(rand() % Anzahl_der_Prozesse);   //aktiven Prozess für CPU setzen
 
-	mmu->set_Seitentabelle(aktiver_Prozess->get_Seitentabelle());  //Die Seitentabelle des aktiven Prozess für die MMU setzen
+    mmu->set_Seitentabelle(aktiver_Prozess->get_Seitentabelle());  //Die Seitentabelle des aktiven Prozess für die MMU setzen
 
-	mmu->get_os()->set_aktiven_Prozess(aktiver_Prozess);   //Den aktiven Prozess für das Betriebssystem setzen
+    mmu->get_os()->set_aktiven_Prozess(aktiver_Prozess);   //Den aktiven Prozess für das Betriebssystem setzen
 
         cout << "#################################" << endl;
         cout << "Neuer aktiver Prozess: " << aktiver_Prozess->get_data_start() << endl;
@@ -32,18 +32,18 @@ CPU::CPU()
 
 CPU::~CPU()
 {
-	delete RAM;
-	delete mmu;
+    delete RAM;
+    delete mmu;
 }
 
 void CPU::operate() 
 {
     for(int i = 0; i < Anzahl_Operationen; i++)
-    {	
-	int operation = rand() % 100 + 1;
+    {   
+    int operation = rand() % 100 + 1;
 
-	if (operation < 70)
-	{
+    if (operation < 70)
+    {
             if (read(Adresse_generieren_delta()))
             {
                 read_operations++;  
@@ -52,10 +52,10 @@ void CPU::operate()
             {
                 cout << endl << "Prozess versucht ausserhalb seines erlaubten Bereiches zu lesen" << endl << endl;
             }
-	}
+    }
 
-	if (operation >= 70 && operation < 90)
-	{
+    if (operation >= 70 && operation < 90)
+    {
             if (write(Adresse_generieren_delta())) 
             {
                 write_operations++;
@@ -64,10 +64,10 @@ void CPU::operate()
             {
                 cout << endl << "Prozess versucht ausserhalb seines erlaubten Bereiches zu schreiben" << endl << endl;
             }
-	}
+    }
 
-	if (operation >= 90)
-	{
+    if (operation >= 90)
+    {
             aktiver_Prozess = Prozesse.at(rand() % Anzahl_der_Prozesse);
 
             mmu->set_Seitentabelle(aktiver_Prozess->get_Seitentabelle()); 
@@ -79,7 +79,7 @@ void CPU::operate()
             cout << "#################################" << endl << endl;
             
             process_change_operations++;
-	}
+    }
         
         usleep(sleep_microseconds);
     }   //Ende for-loop
@@ -87,41 +87,44 @@ void CPU::operate()
 
 bool CPU::write(int adress)
 {
-	if (RAM->bytes[mmu->translate(adress)]->value != aktiver_Prozess->get_stdb()->value)
-	{
+    if (RAM->bytes[mmu->translate(adress)]->value != aktiver_Prozess->get_stdb()->value)
+    {
             write_acess_violations++;
 
             return false;
-	}
-	else 
+    }
+    else 
         {
             cout << "------------------------------------------------------";
             cout << endl << "Schreibe - Operation für die Adresse: " << adress << " erfolgreich" << endl;
             cout << "------------------------------------------------------" << endl << endl << endl;
-	}
-
-	RAM->bytes[mmu->translate(adress)] = aktiver_Prozess->get_writeb();
-        //aktiver_Prozess->get_Seitentabelle()->seiten.at(mmu->translate(adress))->mod = true;   //Modify Bit der aktuell veränderten Seite setzen --> crasht Programm
+    }
         
-	return true;
+        int page = adress / Seitengroesse;
+        
+        aktiver_Prozess->get_Seitentabelle()->seiten.at(page)->mod = true;   //Modify Bit der aktuell veränderten Seite setzen --> crasht Programm
+        RAM->bytes[mmu->translate(adress)] = aktiver_Prozess->get_writeb();
+        //cout << "Mod B: " << aktiver_Prozess->get_Seitentabelle()->seiten.at(mmu->translate(adress))->mod << endl;
+        //cout << "Mod A: " << aktiver_Prozess->get_Seitentabelle()->seiten.at(mmu->translate(adress))->mod << endl;
+    return true;
 }
 
 bool CPU::read(int adress)
 {
-	if (RAM->bytes[mmu->translate(adress)]->value != aktiver_Prozess->get_stdb()->value)
-	{
+    if (RAM->bytes[mmu->translate(adress)]->value != aktiver_Prozess->get_stdb()->value)
+    {
             read_acess_violations++;
 
             return false;
-	}
-	else 
+    }
+    else 
         {
             cout << "------------------------------------------------------";
             cout << endl <<  "Lese - Operation für die Adresse: " << adress << " erfolgreich" << endl;  
             cout << "------------------------------------------------------" << endl << endl << endl;
-	}
+    }
 
-	return true;
+    return true;
 }
 
 int CPU::Adresse_generieren() 
@@ -165,9 +168,9 @@ int CPU::Adresse_generieren_delta()
 {
     int old_adress = 0;
     
-    int value = aktiver_Prozess->get_size() * Seitengroesse;
+    int adress_raum = aktiver_Prozess->get_size() * Seitengroesse;
     
-    int adress = rand() % value;
+    //int adress = rand() % value;
     
     int random = rand() % 100 + 1;
     
@@ -176,8 +179,8 @@ int CPU::Adresse_generieren_delta()
         if(aktiver_Prozess->used_adr.size() > 0)
         {
             old_adress = aktiver_Prozess->used_adr.back();
-            aktiver_Prozess->used_adr.push_back(old_adress + Delta);
-            return old_adress + Delta;
+            aktiver_Prozess->used_adr.push_back(abs((old_adress + Delta) % adress_raum));
+            return (abs((old_adress + Delta) % adress_raum));
         }
         else
             Adresse_generieren();   //Wenn keine Adresse vorhanden, verwende einmal eine zufällige
@@ -187,8 +190,8 @@ int CPU::Adresse_generieren_delta()
          if(aktiver_Prozess->used_adr.size() > 0)
         {
             old_adress = aktiver_Prozess->used_adr.back();
-            aktiver_Prozess->used_adr.push_back(old_adress - Delta);
-            return old_adress - Delta;
+            aktiver_Prozess->used_adr.push_back(abs((old_adress - Delta) % adress_raum));
+            return (abs((old_adress - Delta) % adress_raum));
         }
         else
             Adresse_generieren();   //Wenn keine Adresse vorhanden, verwende einmal eine zufällige   
@@ -201,11 +204,11 @@ void CPU::print() {
 
         cout << "################################################################################" << endl;
         cout << "Schreibe - Operationen: " << write_operations << endl << endl;
-	cout << "Lese - Operationen: " << read_operations << endl << endl;
-	cout << "Prozesswechsel : " << process_change_operations << endl << endl;
-	//cout << "Lesezugriff - versuchte Verletzungen: " << read_acess_violations << endl << endl;
-	//cout << "Schreibzugriff - versuchte Verletzungen: " << write_acess_violations << endl << endl;
-	cout << "Anzahl der entfernten Seiten: " << mmu->get_os()->Seiten_entfernt << endl << endl;
-	cout << "Anzahl der geladenen Seiten: " << mmu->get_os()->Seiten_geladen << endl << endl;
-	cout << "################################################################################" << endl;
+    cout << "Lese - Operationen: " << read_operations << endl << endl;
+    cout << "Prozesswechsel : " << process_change_operations << endl << endl;
+    //cout << "Lesezugriff - versuchte Verletzungen: " << read_acess_violations << endl << endl;
+    //cout << "Schreibzugriff - versuchte Verletzungen: " << write_acess_violations << endl << endl;
+    cout << "Anzahl der entfernten Seiten: " << mmu->get_os()->Seiten_entfernt << endl << endl;
+    cout << "Anzahl der geladenen Seiten: " << mmu->get_os()->Seiten_geladen << endl << endl;
+    cout << "################################################################################" << endl;
 }
